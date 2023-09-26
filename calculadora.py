5b47e87c4983c5d1b12ba8fbde870596bcfa7abb
Mario Garrón Pascual <zx22student3219@colegiovalledelmiro.es>
2023-09-26 13:35:10 +0200

    calculadora

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+#!C:\Users\zx22student3219\AppData\Local\Programs\Python\Python311\python.exe
+
+print("Content-type: text/html\n")
+
+# Suma
+
+def suma():
+    result = 5 + 8
+    return result
+print (suma())
